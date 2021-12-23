8ef3573edefc51827553fe81f69b0725e0f689df
bear-eat-bear-biscuit <153716101@qq.com>
2021-12-23 17:36:40 +0800

    feat: style-mixin新增,引入 elementui

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,10 @@
 <template>
-	<div>Hello Vue2.x</div>
+	<div class="app-wrapper">
+		<!-- 左侧 -->
+		<div>侧边栏</div>
+		<!-- 右侧 -->
+		<div class="main-container"></div>
+	</div>
 </template>
 
 <script>
@@ -9,4 +14,8 @@
 </script>
 
 <style lang='scss' scoped>
+	.app-wrapper {
+		@include clearfix;
+		@include relative;
+	}
 </style>
