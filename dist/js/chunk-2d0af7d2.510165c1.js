10ba081ce8473a1cac41bb974a94c5dc3cc5f3d3
Youzege <youzegehq@gmail.com>
2021-12-28 15:15:41 +0800

    build(dist): 打包

---
+++ dist/js/chunk-2d0af7d2.510165c1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af7d2"],{"0f17":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},a=[],u={name:""},r=u,s=t("2877"),i=Object(s["a"])(r,c,a,!1,null,"0c462256",null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0af7d2.510165c1.js.map
